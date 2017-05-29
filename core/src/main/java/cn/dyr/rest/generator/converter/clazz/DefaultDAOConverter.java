fcc6a7cb8c3c9337773c35eb1cd86dd1d371ceb2
DENG YURONG <532488828@qq.com>
2017-05-29 14:15:47 +0800

    DAO 生成逻辑增加根据维护方实体的唯一标识查询被维护方实体列表的生成

--- core/src/main/java/cn/dyr/rest/generator/converter/clazz/DefaultDAOConverter.java
+++ core/src/main/java/cn/dyr/rest/generator/converter/clazz/DefaultDAOConverter.java
@@ -198,6 +198,60 @@ public class DefaultDAOConverter implements IDAOConverter {
         }
     }
 
+    /**
+     * 本实体为关联关系被维护方，这个方法会添加维护方到被维护方关联关系的查询方法
+     *
+     * @param interfaceClass 类元数据
+     * @param entityInfo     实体数据
+     */
+    private void handledHandledFields(ClassInfo interfaceClass, EntityInfo entityInfo) {
+        ClassInfo entityClass = convertDataContext.getClassByEntityAndType(entityInfo.getName(), TYPE_ENTITY_CLASS);
+        List<ConvertDataContext.RelationshipHandler> handledList = convertDataContext.findByHandled(entityInfo.getName());
+        if (handledList != null) {
+            for (ConvertDataContext.RelationshipHandler handler : handledList) {
+                // 获得维护方的实体类类名、关联字段名称以及维护方的唯一标识符
+                String handlerEntityName = handler.getHandler();
+                String handlerFieldName = handler.getHandlerFieldName();
+
+                ClassInfo handlerEntityClass = convertDataContext.getClassByEntityAndType(handlerEntityName, TYPE_ENTITY_CLASS);
+                String handlerEntityClassNameAsVariable = StringUtils.lowerFirstLatter(handlerEntityName);
+                FieldInfo handlerIdField = ClassInfoUtils.findSingleId(handlerEntityClass);
+
+                // 生成 jpql 语句
+                String jpql = String.format("select %s from %s %s inner join %s.%s %s where %s.%s=?1",
+                        handlerFieldName, handlerEntityClass.getClassName(), handlerEntityClassNameAsVariable,
+                        handlerEntityClassNameAsVariable, handlerFieldName, handlerFieldName,
+                        handlerEntityClassNameAsVariable, handlerIdField.getName());
+
+                // 生成方法名
+                String methodName = String.format("findBy%s%s",
+                        StringUtils.upperFirstLatter(handlerEntityClass.getClassName()),
+                        StringUtils.upperFirstLatter(handlerIdField.getName()));
+
+                // 生成方法
+                MethodInfo getListMethod = new MethodInfo()
+                        .setName(methodName)
+                        .setReturnValueType(CollectionsTypeFactory.listWithGeneric(entityClass.getType()))
+                        .addParameter(ParameterFactory.create(handlerIdField.getType(), "id"))
+                        .setDefineOnly(true)
+                        .addAnnotationInfo(SpringDataAnnotationFactory.query(jpql));
+                interfaceClass.addMethod(getListMethod);
+
+                // 如果已经启用了分页，则同时创建支持分页的查询方法
+                if (converterConfig.isPagingEnabled()) {
+                    MethodInfo getPageMethod = new MethodInfo()
+                            .setName(methodName)
+                            .setReturnValueType(SpringDataTypeFactory.pageTypeWithGeneric(entityClass.getType()))
+                            .addParameter(ParameterFactory.create(handlerIdField.getType(), "id"))
+                            .addParameter(SpringDataParameterFactory.pageable())
+                            .setDefineOnly(true)
+                            .addAnnotationInfo(SpringDataAnnotationFactory.query(jpql));
+                    interfaceClass.addMethod(getPageMethod);
+                }
+            }
+        }
+    }
+
     @Override
     public ClassInfo fromEntity(EntityInfo entityInfo) {
         ClassInfo entityClass = this.convertDataContext.getClassByEntityAndType(entityInfo.getName(), TYPE_ENTITY_CLASS);
@@ -249,6 +303,9 @@ public class DefaultDAOConverter implements IDAOConverter {
 
         // 2.4. 对于在属性信息当中配置好在数据库中会用于数据库查询条件的字段生成相应的方法
         handleExposedFields(interfaceClass, entityInfo);
+
+        // 2.5. 关联关系被维护方生成根据维护方关系查询的方法
+        handledHandledFields(interfaceClass, entityInfo);
 
         return interfaceClass;
     }
